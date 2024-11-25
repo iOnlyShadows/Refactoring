b491cdcb22da50444a9a34e3ca7f3f2f0844e061
iOnlyShadows <matheussimonetto2@gmail.com>
2024-11-25 00:07:09 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -16,26 +16,10 @@ public class Rental {
    }
 
    public double getCharge() {
-       double thisAmount = 0;
-       switch (getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-               thisAmount += 2;
-               if (getDaysRented() > 2)
-                   thisAmount += (getDaysRented() - 2) * 1.5;
-               break;
-           case Movie.NEW_RELEASE:
-               thisAmount += getDaysRented() * 3;
-               break;
-           case Movie.CHILDRENS:
-               thisAmount += 1.5;
-               if (getDaysRented() > 3)
-                   thisAmount += (getDaysRented() - 3) * 1.5;
-               break;
-       }
-       return thisAmount;
+       // Delegando o cálculo para a classe Movie
+       return _movie.getCharge(_daysRented);
    }
 
-   // Novo método extraído e movido
    public int getFrequentRenterPoints() {
        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
            return 2;
