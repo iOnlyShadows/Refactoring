4e0512b438cd028967562f3509b1ab4d9a555918
iOnlyShadows <matheussimonetto2@gmail.com>
2024-11-25 00:58:28 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,20 +1,19 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // Mostrar informações sobre este aluguel
-            result += each.getMovie().getTitle() + ": " +
-                      String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        // Adicionar linhas do rodapé
-        result += "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" +
-                  String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-                  "</EM> frequent renter points<P>";
-        return result;
+    @Override
+    protected String header(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    @Override
+    protected String formatRental(Rental each) {
+        return each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+
+    @Override
+    protected String footer(Customer aCustomer) {
+        return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+               "On this rental you earned <EM>" +
+               String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+               "</EM> frequent renter points<P>";
     }
 }
--- Statement.java
+++ Statement.java
@@ -1,3 +1,20 @@
-public class Statement {
-    
+import java.util.Enumeration;
+
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+        String result = header(aCustomer);
+        Enumeration rentals = aCustomer.getRentals();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += formatRental(each);
+        }
+        result += footer(aCustomer);
+        return result;
+    }
+
+    protected abstract String header(Customer aCustomer);
+
+    protected abstract String formatRental(Rental each);
+
+    protected abstract String footer(Customer aCustomer);
 }
