4d3778c96ef42a580e63ac7ffc3b18d0b2aa2683
iOnlyShadows <matheussimonetto2@gmail.com>
2024-11-25 00:36:14 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,10 +1,8 @@
 public abstract class Price {
     public abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        // Implementação genérica, pode ser sobrescrita nas subclasses
-        return 0;
-    }
+    // Tornando o método getCharge abstrato
+    public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
         return 1; // Valor padrão
